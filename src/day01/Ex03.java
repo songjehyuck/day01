dfda4a7445d74c34334adf52f3d55e7a49b8b86f
jehyuck <zzukk78@naver.com>
2021-07-23 13:02:22 +0900

    숫자 문자열 연산

--- src/day01/Ex03.java
+++ src/day01/Ex03.java
@@ -1,5 +1,12 @@
 package day01;
 
 public class Ex03 {
-
+	public static void main(String[] args) {
+		System.out.println(100 + 200);
+		System.out.println("문자열"+100);
+		System.out.println("문자열"+100+500);
+		System.out.println("문자열"+(100+500));
+		System.out.println("100"+100);
+		
+	}
 }
